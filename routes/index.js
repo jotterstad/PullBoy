a0f46408dda8193b04b5e5abf254bfe8caeabe05
Your Name <you@example.com>
2017-04-20 19:47:42 -0500

    index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
+var path = require('path');
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next){
+	res.sendStatus(200);
+	next();
+},
+
+function (req, res, next){
+		require('simple-git')('/path/to/project')
+ 		.pull()
+ 		.then(function() {
+        	console.log('pull done.')
+    	});
+}
+
+);
 module.exports = router;
